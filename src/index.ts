0373638506193b78de54e9beeaaafeab05a6d932
Calvin Lau <calvin@mona.co>
2019-10-21 15:34:16 +0800

    📄 Updated license wordings

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-//! # Settlement engine runner
 //! adapted from https://github.com/interledgerjs/settlement-xrp (XRP On-Ledger Settlement Engine)
 //! Copyright Copyright (c) 2019 Interledger Team (licensed under the Apache License, Version 2.0)
 //! Modifications Copyright (c) 2018 - 2019, Foris Limited (licensed under the Apache License, Version 2.0)
